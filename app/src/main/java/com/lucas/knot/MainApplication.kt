4858ec1083756550dbb68f031fe2e2f1e1b4117d
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-12-17 01:20:37 +0800

    MainApplication.kt: configure dependency injection for the rest of the services

--- app/src/main/java/com/lucas/knot/MainApplication.kt
+++ app/src/main/java/com/lucas/knot/MainApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.ProgressDialog
 import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import dagger.Module
@@ -15,13 +16,14 @@ import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.grpc.ManagedChannelBuilder
-import services.ChatGrpc
-import services.IdentityGrpc
-import services.UserGrpc
+import services.*
 import javax.inject.Singleton
+import services.AdvertisingGrpc.newBlockingStub as newAdvertisingStub
 import services.ChatGrpc.newBlockingStub as newChatStub
 import services.ChatGrpc.newStub as newChatNormalStub
 import services.IdentityGrpc.newBlockingStub as newIdentityStub
+import services.NotificationGrpc.newBlockingStub as newNotificationStub
+import services.SignalingGrpc.newStub as newSignalingStub
 import services.UserGrpc.newBlockingStub as newUserStub
 
 @HiltAndroidApp
@@ -33,6 +35,7 @@ class MainApplication : Application()
 object NetworkModule {
     private const val baseUrl = "10.0.2.2"
 
+    //TODO TLS
     @Provides
     fun providesIdentityService(): IdentityGrpc.IdentityBlockingStub {
         val channel = ManagedChannelBuilder.forAddress(baseUrl, 5001)
@@ -50,7 +53,7 @@ object NetworkModule {
     }
 
     @Provides
-    fun providdesAsyncChatService(): ChatGrpc.ChatStub {
+    fun providesAsyncChatService(): ChatGrpc.ChatStub {
         val channel = ManagedChannelBuilder.forAddress(baseUrl, 7001)
                 .usePlaintext()
                 .build()
@@ -65,14 +68,45 @@ object NetworkModule {
         return newUserStub(channel)
     }
 
+    @Provides
+    fun providesNotificationService(): NotificationGrpc.NotificationBlockingStub {
+        val channel = ManagedChannelBuilder.forAddress(baseUrl, 11001)
+                .usePlaintext()
+                .build()
+        return newNotificationStub(channel)
+    }
+
+    // provide normal stub for streaming
+    @Provides
+    fun providesSignalingService(): SignalingGrpc.SignalingStub {
+        val channel = ManagedChannelBuilder.forAddress(baseUrl, 10001)
+                .usePlaintext()
+                .build()
+        return newSignalingStub(channel)
+    }
+
+    @Provides
+    fun providesAdvertisingService(): AdvertisingGrpc.AdvertisingBlockingStub {
+        val channel = ManagedChannelBuilder.forAddress(baseUrl, 9001)
+                .usePlaintext()
+                .build()
+        return newAdvertisingStub(channel)
+    }
+
     // although firebase classes are singletons, it is nicer to just use constructor injection
     @Provides
     fun providesFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
     }
 
+    @Provides
     fun providesFirebaseStorage(): FirebaseStorage {
         return FirebaseStorage.getInstance()
+    }
+
+    @Provides
+    fun providesFirebaseMessaging(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
     }
 }
 
